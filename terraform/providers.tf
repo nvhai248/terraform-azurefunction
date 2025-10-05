44960eca9e30c1591d71c511d78002f9195d839d
haiml248 <hainguyen@microleap.org>
2025-10-05 16:44:57 +0700

    update ci

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,15 +11,15 @@ terraform {
 
 provider "azurerm" {
   features {}
-  # subscription_id = var.subscription_id
-  # tenant_id       = var.tenant_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 
 }
 
 provider "azuread" {
-  # tenant_id     = var.tenant_id
-  # client_id     = var.client_id
-  # client_secret = var.client_secret
+  tenant_id     = var.tenant_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
 }
